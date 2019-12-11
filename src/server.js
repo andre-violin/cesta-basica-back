0d938c2344c2ef781d669a237b14b83c2b8c2bc3
Andre Violin <andreviolin@gmail.com>
2019-12-11 10:54:04 -0400

    set environment variable

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const routes = require("./routes");
 
-mongoose.connect("mongodb://localhost:27017/cesta-basica", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
